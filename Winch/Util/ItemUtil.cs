a3a7f289786424b2ea58b924eeb630466adb9ab9
bdlm-dev <74147859+bdlm-dev@users.noreply.github.com>
2023-05-13 16:56:48 +0100

    Patch to replace fish in POI, Assign default particle to new items

--- Winch/Util/ItemUtil.cs
+++ Winch/Util/ItemUtil.cs
@@ -1,7 +1,9 @@
-﻿using Newtonsoft.Json;
+﻿using DG.Tweening.Core.Easing;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using Winch.Core;
+using Winch.Core.API.Events.Addressables;
 using Winch.Serialization;
 using Winch.Serialization.Item;
 
@@ -25,21 +27,27 @@ internal static class ItemUtil
         { typeof(DamageItemData), new DamageItemDataConverter() },
     };
 
+    public static UnityEngine.GameObject? defaultHarvestParticlePrefab;
+
     public static Dictionary<string, ItemData> harvestableItemDataDict = new();
     public static Dictionary<string, ItemData> allItemDataDict = new();
+    public static Dictionary<string, ItemData> itemsToReplace = new();
 
     public static void PopulateItemData()
     {
+        if (allItemDataDict.Count > 0) return;
         foreach (var item in GameManager.Instance.ItemManager.allItems)
         {
             if (item is FishItemData or RelicItemData or HarvestableItemData)
             {
                 harvestableItemDataDict.Add(item.id, item);
-                WinchCore.Log.Debug($"Added item {item.id} to harvestableItemDataDict");
+                //WinchCore.Log.Debug($"Added item {item.id} to harvestableItemDataDict");
             }
             allItemDataDict.Add(item.id, item);
-            WinchCore.Log.Debug($"Added item {item.id} to allItemDataDict");
+            //WinchCore.Log.Debug($"Added item {item.id} to allItemDataDict");
         }
+
+        ReplaceItems();
     }
 
     internal static void AddItemFromMeta<T>(string metaPath) where T : ItemData
@@ -50,16 +58,47 @@ internal static class ItemUtil
             WinchCore.Log.Error($"Meta file {metaPath} is empty");
             return;
         }
-        if (allItemDataDict.ContainsKey((string)meta["id"]))
+        var itemName = (string)meta["id"];
+        if (allItemDataDict.ContainsKey(itemName))
         {
-            WinchCore.Log.Error($"Duplicate item {(string)meta["id"]} at {metaPath} failed to load");
-            return;
+            if (meta.ContainsKey("doReplaceOriginal") && (bool)meta["doReplaceOriginal"]) { }
+            else
+            {
+                WinchCore.Log.Error($"Duplicate item {itemName} at {metaPath} failed to load");
+                return;
+            }
         }
         var item = UtilHelpers.GetScriptableObjectFromMeta<T>(meta, metaPath);
         if (UtilHelpers.PopulateObjectFromMeta<T>(item, meta, Converters))
         {
-            GameManager.Instance.ItemManager.allItems.Add(item);
-            allItemDataDict.Add(item.id, item);
+            if (meta.ContainsKey("doReplaceOriginal") && (bool)meta["doReplaceOriginal"])
+            {
+                itemsToReplace.Add(item.id, item);
+                WinchCore.Log.Info($"Added {item.id} to itemsToReplace");
+            }
+            else
+            {
+                GameManager.Instance.ItemManager.allItems.Add(item);
+            }
+        }
+    }
+
+    internal static void ReplaceItems()
+    {
+        WinchCore.Log.Info($"Attempting to replace {itemsToReplace.Count} item{(itemsToReplace.Count > 1 ? "s" : "")} in allItems.");
+        try
+        {
+            for(int i = 0; i < GameManager.Instance.ItemManager.allItems?.Count; i++) 
+            {
+                if (itemsToReplace.ContainsKey(GameManager.Instance.ItemManager.allItems[i].id))
+                {
+                    GameManager.Instance.ItemManager.allItems[i] = itemsToReplace[GameManager.Instance.ItemManager.allItems[i].id];
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            WinchCore.Log.Debug($"Error replacing items in allItems: {ex}");
         }
     }
 }
